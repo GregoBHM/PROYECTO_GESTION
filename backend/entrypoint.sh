017ca3ecd95dd31e1f7493c87c24e93f3d823319
GregoBHM <grehuanca@upt.pe>
2026-05-30 23:42:14 -0500

    Agregar makemigrations automáticos al entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,7 @@ while ! nc -z $DB_HOST $DB_PORT; do
   sleep 0.5
 done
 
+python manage.py makemigrations users projects tickets schedule notifications --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py loaddata initial_roles.json 2>/dev/null || true
